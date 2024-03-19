83d5fb3cbb4b1a5455c0f1eae18ac728a88484c1
Azevedo183 <armandojpazevedo@gmail.com>
2024-03-19 00:35:05 +0000

    rollback

--- script.js
+++ script.js
@@ -11,39 +11,8 @@ function displayCombination(combination) {
 document.getElementById('calculateButton').addEventListener('click', calculateCombination);
 
 function calculateCombination() {
-  var actionValues = {
-    'HIT LIGHT': -3,
-    'HARD HIT': -5,
-    'MEDIUM HIT': -6,
-    'DRAW': -15,
-    'PUNCH': 2,
-    'BEND': 7,
-    'UPSET': 13,
-    'SHRINK': 16
-  };
+  var points = parseInt(document.getElementById('points').value);
 
-  var totalPoints = 0;
-  var selectedActions = [];
-
-  // Obter os valores das ações selecionadas e somá-los
-  for (var i = 1; i <= 3; i++) {
-    var actionId = 'action' + i;
-    var selectedAction = document.getElementById(actionId).value;
-    selectedActions.push(selectedAction);
-    totalPoints += actionValues[selectedAction];
-  }
-
-  var targetValue = parseInt(document.getElementById('points').value);
-  var combination = findCombination(totalPoints, targetValue);
-
-  if (combination) {
-    displayCombination(combination);
-  } else {
-    document.getElementById('result').innerHTML = "Não há combinação de ações para atingir o valor " + targetValue;
-  }
-}
-
-function findCombination(totalPoints, targetValue) {
   var availableActions = {
     'HIT LIGHT': -3,
     'HARD HIT': -5,
@@ -57,6 +26,7 @@ function findCombination(totalPoints, targetValue) {
 
   var combination = {};
 
+  // Ordenar as ações disponíveis pelos seus valores para começar com as ações que têm o maior impacto
   var sortedActions = Object.keys(availableActions).sort(function(a, b) {
     return availableActions[b] - availableActions[a];
   });
@@ -65,10 +35,15 @@ function findCombination(totalPoints, targetValue) {
     var action = sortedActions[i];
     var actionValue = availableActions[action];
 
-    var count = Math.floor(targetValue / actionValue);
+    var count = Math.floor(points / actionValue);
     combination[action] = count;
-    targetValue -= count * actionValue;
+    points -= count * actionValue;
   }
 
-  return targetValue === 0 ? combination : null;
+  if (points === 0) {
+    displayCombination(combination);
+  } else {
+    document.getElementById('result').innerHTML = "Não há combinação de ações para atingir o valor " + points;
+  }
 }
+
